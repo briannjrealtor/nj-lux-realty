b060e4a1b461cc607b87f5f9ebefa7727d1facf3
Brian DeMarco <briannjrealtor@gmail.com>
2026-03-21 19:42:29 -0400

    force render test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const App = () => {
+  return <h1 style={{ padding: 40 }}>IT WORKS 🚀</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
